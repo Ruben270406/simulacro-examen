c00c0dde6c535f52a43577f896ca7c349359a87e
Ruben <rvidabravo@gmail.com>
2025-11-24 10:19:49 +0100

    Genre

--- client/src/main.js
+++ client/src/main.js
@@ -1,22 +1,16 @@
 import './style.css'
-import viteLogo from '/vite.svg'
+import './classes/genre.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.addEventListener('DOMContentLoaded', async () => {
+	const genreSelect = document.querySelector('.selectors-container select')
+	if (!genreSelect) return
 
-setupCounter(document.querySelector('#counter'))
+	try {
+		const genres = await window.fetchGenres('http://localhost:3000/genres')
+		// fetchGenres devuelve una promesa que resuelve en Genre[] (usa parseGenres internamente)
+		window.fillGenres(genres, genreSelect, 'Select genre')
+	} catch (err) {
+		console.error('No se pudieron cargar los géneros:', err)
+	}
+})
+
